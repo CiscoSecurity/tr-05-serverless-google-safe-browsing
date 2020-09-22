f7661632ed7222af4e64a9590776ff78dfeffc0f
mmyskiv <mmysk@softserveinc.com>
2020-09-22 14:56:33 +0300

    Removed ugly fix of circular imports with help of current_app

--- api/utils.py
+++ api/utils.py
@@ -64,9 +64,6 @@ def jsonify_data(data):
 
 
 def jsonify_errors(error, data=None):
-    # Avoiding of circular imports
-    from app import app
-
     # Make the actual GSB error payload compatible with the expected TR error
     # payload in order to fix the following types of possible UI alerts, e.g.:
     # :code (not (instance? java.lang.String 40x)),
@@ -86,6 +83,6 @@ def jsonify_errors(error, data=None):
     if data:
         payload['data'] = data
 
-    app.logger.error(payload)
+    current_app.logger.error(payload)
 
     return jsonify(payload)
