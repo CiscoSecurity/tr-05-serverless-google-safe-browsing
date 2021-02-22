1f813fc1cbaa86a26416c2997d250d814bff3d85
anastasiia.rozlyvan <nastia.2.r.2@gmail.com>
2021-02-22 11:51:53 +0200

    Fix error message and mock

--- code/tests/unit/api/mock_for_tests.py
+++ code/tests/unit/api/mock_for_tests.py
@@ -106,7 +106,7 @@ EXPECTED_WATCHDOG_ERROR = {
     'errors': [
         {
             'code': 'health check failed',
-            'message': 'Invalid Health Check',
+            'message': 'Invalid Health Check.',
             'type': 'fatal'
         }
     ]
